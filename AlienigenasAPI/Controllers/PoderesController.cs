04cabf0495f55523e38b5b0a5e89ed05953aa26e
Caio Diego <caiodsj96@gmail.com>
2023-08-30 14:23:09 -0300

    criação serviço e controller do 'poder'

---
+++ AlienigenasAPI/Controllers/PoderesController.cs
@@ -0,0 +1,51 @@
+﻿using AlienigenasAPI.DTOs;
+using AlienigenasAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlienigenasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PoderesController : ControllerBase
+    {
+        private readonly IPoderService _poderService;
+
+        public PoderesController(IPoderService poderService)
+        {
+            _poderService = poderService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Poder>>> GetAllPoderes()
+        {
+            return Ok(await _poderService.GetAllPoderes());
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Poder>> GetPoderById(int id)
+        {
+            var poder = await _poderService.GetPoderPorId(id);
+            if (poder == null) return NotFound("Nenhum poder encontrado");
+            return Ok(poder);
+        }
+        [HttpPost]
+        public async Task<ActionResult> CreatePoder(PoderDTO poder)
+        {
+            await _poderService.CreatePoder(poder);
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdatePoder(int id, PoderDTO request)
+        {
+            await _poderService.UpdatePoder(id, request);
+            return Ok($"O poder {request.Nome} foi atualizado");
+        }
+        [HttpDelete]
+        public async Task<ActionResult> DeletePoder(int id)
+        {
+            await _poderService.RemovePoder(id);
+            return Ok("Poder excluído com sucesso.");
+        }
+        
+    }
+}
---
+++ AlienigenasAPI/Services/PoderService.cs
@@ -0,0 +1,54 @@
+﻿using AlienigenasAPI.DTOs;
+using AlienigenasAPI.Models;
+
+namespace AlienigenasAPI.Services
+{
+    public class PoderService : IPoderService
+    {
+        private readonly DataContext _dataContext;
+
+        public PoderService(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<List<Poder>> GetAllPoderes()
+        {
+            return await _dataContext.Poderes.ToListAsync();
+        }
+
+        public async Task<Poder> GetPoderPorId(int id)
+        {
+            return await _dataContext.Poderes.FindAsync(id);
+        }
+        public async Task<string> CreatePoder(PoderDTO request)
+        {
+            var poder = new Poder
+            {
+                Nome = request.Nome,
+                Descricao = request.Descricao
+            };
+            await _dataContext.Poderes.AddAsync(poder);
+            await _dataContext.SaveChangesAsync();
+            return $"O poder '{poder.Nome}' foi criado com sucesso.";
+        }
+
+        public async Task<string> UpdatePoder(int id, PoderDTO request)
+        {
+            var poder = await _dataContext.Poderes.FindAsync(id);
+            if (poder == null) return null;
+            poder.Nome = request.Nome;
+            poder.Descricao = request.Descricao;
+            await _dataContext.SaveChangesAsync();
+            return $"'{poder.Nome}' foi atualizado com sucesso.";
+        }
+        public async Task<string> RemovePoder(int id)
+        {
+            var poder = await _dataContext.Poderes.FindAsync(id);
+            if (poder == null) return null;
+            _dataContext.Poderes.Remove(poder);
+            await _dataContext.SaveChangesAsync();
+            return $"O poder '{poder.Nome}' foi excluído com sucesso.";
+        }
+    }
+}
