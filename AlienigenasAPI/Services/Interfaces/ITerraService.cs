384e3ab71b06411a66f1c6596882177eb68941e5
Raul <raul.rhf9316@gmail.com>
2023-08-30 16:12:12 -0300

    add filtros

--- AlienigenasAPI/Services/Interfaces/ITerraService.cs
+++ AlienigenasAPI/Services/Interfaces/ITerraService.cs
@@ -6,6 +6,8 @@ namespace AlienigenasAPI.Services.Interfaces
     {
         Task<List<Alien>> GetAllAliensDaTerra();
         Task<List<Alien>> GetAllAliensPorPlaneta(int idPlaneta);
+        Task<List<Alien>> GetAlienNaTerraPorEspecie(string especie);
+        Task<List<Alien>> GetAlienNaTerraPorNome(string nomeAlien);
         Task<Alien> GetAlienNaTerraPorId(int id);
         Task<string> EntrarNaTerra(int id);
         Task<string> SairDaTerra(int id);
