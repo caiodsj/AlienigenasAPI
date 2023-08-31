345a8747689b0f03112dda151a779c626975611d
Paulo Lisboa <paulolisboa38@gmail.com>
2023-08-31 03:17:13 -0300

    DataAnnotations classe PlanetaDTO.cs

--- AlienigenasAPI/DTOs/PlanetaDTO.cs
+++ AlienigenasAPI/DTOs/PlanetaDTO.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AlienigenasAPI.DTOs
 {
     public class PlanetaDTO
     {
+        [Required]
+        [StringLength(100)]
         public string Nome { get; set; }
+
+        [Required]
+        [StringLength(200)]
         public string Descricao { get; set; }
+
+        [Range(0,int.MaxValue)]
         public int Populacao { get; set; }
     }
 }
