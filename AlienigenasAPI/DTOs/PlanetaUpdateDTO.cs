6c00ff52e9bf0459b47faf65b714bc1e4a5e8f02
Paulo Lisboa <paulolisboa38@gmail.com>
2023-08-31 03:15:05 -0300

    Criação da classe PlanetaUpdateDTO.cs

---
+++ AlienigenasAPI/DTOs/PlanetaUpdateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlienigenasAPI.DTOs
+{
+    public class PlanetaUpdateDTO
+    {
+        [Required]
+        [StringLength(200)]
+        public string Descricao { get; set; }
+
+        [Range(0,int.MaxValue)]
+        public int Populacao { get; set; }
+    }
+}
