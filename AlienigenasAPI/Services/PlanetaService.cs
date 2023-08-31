3acc568d078d054835bd771113530904b9f7f64e
Paulo Lisboa <paulolisboa38@gmail.com>
2023-08-31 03:21:07 -0300

    Criação método GetAllPlanetasPorNomeAsync

--- AlienigenasAPI/Services/PlanetaService.cs
+++ AlienigenasAPI/Services/PlanetaService.cs
@@ -1,5 +1,7 @@
 ﻿using AlienigenasAPI.Models;
 using AlienigenasAPI.DTOs;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Globalization;
 
 namespace AlienigenasAPI.Services
 {
@@ -10,6 +12,32 @@ namespace AlienigenasAPI.Services
         public PlanetaService(DataContext context)
         {
             _dataContext = context;
+        }
+
+        public async Task<List<Planeta>> GetAllPlanetasAsync()
+        {
+            return await _dataContext.Planetas.ToListAsync();
+        }
+
+        public async Task<Planeta> GetPlanetaPorIdAsync(int id)
+        {
+            return await _dataContext.Planetas.FindAsync(id);
+        }
+
+        public async Task<List<Planeta>> GetAllPlanetasPorNomeAsync(string nome)
+        {
+            var planetas = await _dataContext.Planetas
+                .Where(p => p.Nome.Contains(nome))
+                .ToListAsync();
+            return planetas;
+        }
+
+        public async Task<bool> DoesPlanetaExistsAsync(string nome)
+        {
+            var planetaExistente = await _dataContext.Planetas
+                .Where(p => p.Nome == nome)
+                .FirstOrDefaultAsync();
+            return planetaExistente != null;
         }
 
         public async Task<Planeta> CreatePlanetaAsync(PlanetaDTO request)
@@ -25,24 +53,13 @@ namespace AlienigenasAPI.Services
             return planeta;
         }
 
-        public async Task<List<Planeta>> GetAllPlanetasAsync()
-        {
-            return await _dataContext.Planetas.ToListAsync();
-        }
-
-        public async Task<Planeta> GetPlanetaPorIdAsync(int id)
-        {
-            return await _dataContext.Planetas.FindAsync(id);
-        }
-
-        public async Task<Planeta> UpdatePlanetaAsync(int id,PlanetaDTO request)
+        public async Task<Planeta> UpdatePlanetaAsync(int id,PlanetaUpdateDTO request)
         {
             var planeta = await _dataContext.Planetas.FindAsync(id);
             if (planeta == null)
             {
                 return null;
             }
-            planeta.Nome = request.Nome;
             planeta.Descricao = request.Descricao;
             planeta.Populacao = request.Populacao;
             await _dataContext.SaveChangesAsync();
